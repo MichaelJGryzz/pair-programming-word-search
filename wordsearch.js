6181f2a5597d9ffbdfcc84507fdb54f4e6cabc78
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-25 19:47:09 -0400

    Modify the "wordSearch" function to handle if the word is written vertically.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
 const wordSearch = (letters, word) => {
   const horizontalJoin = letters.map(ls => ls.join(''));
-  for (l of horizontalJoin) {
+  for (const l of horizontalJoin) {
     if (l.includes(word)) return true;
   }
+
+  const numRows = letters.length;
+  const numColumns = letters[0].length;
+  let verticalJoin = Array(numColumns).fill('');
+  for (let row = 0; row < numRows; row++) {
+    for (let column = 0; column < numColumns; column++) {
+      verticalJoin[column] += letters[row][column];
+    }
+  }
+  for (const v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+
   return false;
 };
 
